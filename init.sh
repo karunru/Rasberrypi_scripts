f58becd3b5d0ae87c92709c6186c72811a43a10f
karunru <1qazhikaru@gmail.com>
2016-09-30 15:31:37 +0900

    chromiumからepiphanyに変更

--- init.sh
+++ init.sh
@@ -11,13 +11,13 @@ sed -i -e 's/#disable_overscan=1/disable_overscan=1/g' /boot/config.txt
 # chromiumのインストール
 # ここ(https://www.raspberrypi.org/forums/viewtopic.php?t=121195)を参考にした
 
-wget -qO - http://bintray.com/user/downloadSubjectPublicKey?username=bintray | sudo apt-key add -
-echo "deb http://dl.bintray.com/kusti8/chromium-rpi jessie main" | sudo tee -a /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get install chromium-browser -y
+# wget -qO - http://bintray.com/user/downloadSubjectPublicKey?username=bintray | sudo apt-key add -
+# echo "deb http://dl.bintray.com/kusti8/chromium-rpi jessie main" | sudo tee -a /etc/apt/sources.list
+# sudo apt-get update
+# sudo apt-get install chromium-browser -y
 
-### 日本語フォントのインストール
-sudo apt-get install fonts-mplus xfonts-mplus -y
+### xdotool,日本語フォントのインストール
+sudo apt-get install xdotool fonts-mplus xfonts-mplus -y
 
 ### スクリーンセーバーを起動させない
 sed -i -e 's/@xscreensaver/#@xscreensaver/g' /home/pi/.config/lxsession/LXDE-pi/autostart
@@ -27,6 +27,6 @@ echo -e '@xset s off\n	\
 				 @xset s no-blank\n' >> /home/pi/.config/lxsession/LXDE-pi/autostart
 
 ### chromiumを起動時に自動起動させる
-echo '@/usr/bin/chromium-browser --noerrdialog --kiosk --incognito http://nandakke.oedalab.jp/ 1>/dev/null 2>/dev/null' >> /home/pi/.config/lxsession/LXDE-pi/autostart
+echo '@/home/pi/Raspberrypi_scripts/fullscreen.sh' >> /home/pi/.config/lxsession/LXDE-pi/autostart
 echo '@xdotool mousemove 0 1081' >> /home/pi/.config/lxsession/LXDE-pi/autostart
 
