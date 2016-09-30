3380405a16af46508d16d1b0c5ddbae39e7ef81c
karunru <1qazhikaru@gmail.com>
2016-09-30 15:19:12 +0900

    URLをnandakkeに変更

--- fullscreen.sh
+++ fullscreen.sh
@@ -1,1 +1,1 @@
-sudo -u pi epiphany-browser -a -i --profile ~/.config [URL] --display=:0 & sleep 15s; xdotool key F11
+sudo -u pi epiphany-browser -a -i --profile ~/.config http://nandakke.oedalab.jp/ --display=:0 & sleep 15s; xdotool key F11
